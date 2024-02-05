af740e924276ecc7b1d8a432918da9ee057ef5e5
Yuliia <the.smile.too@gmail.com>
2024-02-05 19:56:21 +0200

    adding functions

--- src/js/script.js
+++ src/js/script.js
@@ -7,8 +7,30 @@ const output = document.getElementById("output");
 let isError = false;
 
 function cleanInputString(str) {
-  const strArray = str.split("");
-  const cleanStrArray = [];
+  // const strArray = str.split("");
+  // const cleanStrArray = [];
+  // for (let i = 0; i < strArray.length; i++) {
+  //   if (!["+", "-", " "].includes(strArray[i])) {
+  //     cleanStrArray.push(strArray[i]);
+  //   }
+  // }
+}
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
+}
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
 
-  for (let i = 0; i < strArray.length; i++) {}
+function addEntry() {
+  const targetInputContainer = document.querySelector(
+    `#${entryDropdown.value} .input-container`
+  );
+  const entryNumber =
+    targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>`;
 }
